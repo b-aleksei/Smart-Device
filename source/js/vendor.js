91c975313a08967ee87816be2c55ad7e1ad7bd38
Aleksei Bugaev <103301@mail.ru>
2020-08-22 15:22:49 +0300

    добавляет хедер

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,6 +1,3 @@
-// import {ieFix} from './vendor/polyfills';
-// import './vendor/picturefill.min';
-// import './vendor/focus-visible.min';
-// import './vendor/ArrayFromPolyfill.min';
-//
-// ieFix();
+import {ieFix} from './vendor/polyfills';
+import './vendor/picturefill.min';
+ieFix();
